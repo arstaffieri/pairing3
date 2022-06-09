df5934552a16dc16c0a5a0d6c267eaff19e96102
Angie Staffieri <angie.staffieri@gmail.com>
2022-06-09 15:21:18 -0600

    objectLiteralPractice.js

---
+++ objectLiteralPractice.js
@@ -0,0 +1,10 @@
+var person = {
+  name1: "Hazel",
+  name2: "Angie",
+  name3: "Shaya",
+  name4: "Jacob"
+}
+console.log(person);
+console.log(person.name1);
+console.log(person.name5 = "Bob");
+console.log(person.name6 = "Jane");
